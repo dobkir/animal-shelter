258e5a72a128536967ec38123e387fe7c73b529d
dobkir <dobkir@gmail.com>
2022-04-15 21:15:58 +0400

    feat: add header section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 "use strict"
 console.log("index.js has been started")
+//============ Active nav link on the corresponding page ============//
+
+const navLink = document.querySelectorAll(".navigation__item-link")
+
+navLink.forEach(link => {
+  if ((window.location.pathname === "/" && link.href.match(/index.html/)) ||
+    (link.href === window.location.href)) {
+    link.classList.add('active-link')
+  }
+})
+
+//========= End of active nav link on the corresponding page ========//
