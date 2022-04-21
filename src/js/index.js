f4f4a9d6b54ab645a008c77ea7f570963ea59684
dobkir <dobkir@gmail.com>
2022-04-21 12:30:39 +0400

    refactor: improve JavaScript link handling code

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 "use strict"
 console.log("index.js has been started")
+
+
+
 //============ Active nav link on the corresponding page ============//
 
 const navLink = document.querySelectorAll(".navigation__item-link")
@@ -11,13 +14,6 @@ navLink.forEach(link => {
   }
 })
 
-// if (window.location.hash) {
-//   window.location.pathname === '/'
-//   window.location.hash === ''
-//   console.log(window.location.hash)
-//   console.log(window.location)
-// }
-
 //========= End of active nav link on the corresponding page ========//
 
 
@@ -25,49 +21,52 @@ navLink.forEach(link => {
 //================== Smooth scrolling of pages ==================//
 //========== Clear the hash links in a window location ==========//
 
-// The href attribute must contain the hash
-const hashLinksHome = document.querySelectorAll('a[href^="#"]:not(a[href="#"])')
-const hashLinksOthers = document.querySelectorAll('a[href^="./#"]:not(a[href="./#"])')
+const navLinks = document.querySelectorAll('.navigation__item-link')
 
-// Handle links with hashes of the main page
-// event.preventDefault() here stop the moving hash to window location
-hashLinksHome.forEach(link => {
+/**
+ * Handle links with hashes of the main page
+ * If it's a homepage, event.preventDefault() stop the moving hash to window location.
+ * If it isn't a home page, the removeHashFromLocation() function 
+ * removes the hash from a location after windows loading.
+ */
 
-  link.addEventListener('click', (event) => {
-    // Stop to move hash into window location
-    event.preventDefault()
+navLinks.forEach(link => {
 
-    // Get the link after a hash
-    const id = link.getAttribute('href').substring(1)
-    // Smooth scrolling to this hash link
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth'
+  if (link.hash) {
+
+    link.addEventListener('click', (event) => {
+
+      // If there aren't symbols before the hash, that means that's a homepage.
+      const isHome = !link.getAttribute('href').split(/#/)[0]
+
+      // Get the link after a hash
+      const id = link.hash.replace(/#/, '')
+
+      if (isHome) {
+        // Stop to move hash into window location
+        event.preventDefault()
+      }
+
+      // Smooth scrolling to this hash link
+      document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
+      })
     })
-  })
-})
-
-// Handle links with hashes for all other pages except the main one
-hashLinksOthers.forEach(link => {
-  link.addEventListener('click', (event) => {
-
-    const id = link.getAttribute('href').substring(3)
-
-    // Smooth scrolling to this hash link, when go to another page
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
-
+  }
 })
 
 // Remove hash link from a window location, after the window load or reload
-window.addEventListener('load', () => {
-  if (window.history.pushState) {
-    window.history.pushState('', '/', window.location.pathname)
-  } else {
-    window.location.hash = '';
-  }
-})
+function removeHashFromLocation() {
+  window.addEventListener('load', () => {
+    if (window.history.pushState) {
+      window.history.pushState('', '/', window.location.pathname)
+    } else {
+      window.location.hash = '';
+    }
+  })
+}
+
+removeHashFromLocation()
 
 //======= End of Clear the hash links in a window location =======//
 //=============== End of Smooth scrolling of pages ===============//
