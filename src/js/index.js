1e520b02fc14d3f77e176ca269fca8ca5c9839a7
dobkir <dobkir@gmail.com>
2022-04-23 22:41:27 +0400

    refactor: refactor all code before pull request
    Remove default active link handler from index.js and make them in navigation.hbs
    Add a new placeholder for repeating scss strings %flex-column-space-btw

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,4 @@
 "use strict"
-console.log("index.js has been started")
-
-
-
-//============ Active nav link on the corresponding page ============//
-
-const navLink = document.querySelectorAll(".navigation__item-link")
-
-navLink.forEach(link => {
-  if ((window.location.pathname === "/" && link.href.match(/index.html/)) ||
-    (link.href === window.location.href && !link.href.match(/[\?#]/))) {
-    link.classList.add('active-link')
-  }
-})
-
-//========= End of active nav link on the corresponding page ========//
-
-
 
 //================== Smooth scrolling of pages ==================//
 //========== Clear the hash links in a window location ==========//
